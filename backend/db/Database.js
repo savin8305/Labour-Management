897002978805ac35cc7ec388480b1a778fbc639b
savin8305 <kanhaji0402@gmail.com>
2023-09-27 01:09:49 +0530

    initial commit to this project

---
+++ backend/db/Database.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+console.log(process.env.DB_URL);
+const connectDatabase = () => {
+  mongoose
+    .connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then((data) => {
+      console.log(`mongod connected with server: ${data.connection.host}`);
+    });
+};
+
+module.exports = connectDatabase;
